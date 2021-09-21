b65c50c9f0b2337bf71693a6e7efa9be98af8f10
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2021-09-21 21:25:24 +0300

    create: toggle slider with keyboard

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+  },
   // Optional parameters
   loop: true,
+
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
+
 });
