80267313e8ea6fa4f5bb3ae8f1d87773efa4ecbe
Le5ya <49882766+Le5ya@users.noreply.github.com>
2021-09-26 22:12:56 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
-const swiper = new Swiper('.swiper', {
+const hotelSlider = new Swiper('.hotel-slider', {
   slidesPerView: 1,
   spaceBetween: 30,
   keyboard: {
-    enabled: true,
+  enabled: true,
   },
   // Optional parameters
   loop: true,
-
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+  effect: "coverflow",
+});
+const reviewsSlider = new Swiper('.reviews-slider', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  keyboard: {
+  enabled: true,
   },
 
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+  // effect: "coverflow",
 });
+$('.parallax-window').parallax({imageSrc: './img/newsletter-bg.jpeg'});
